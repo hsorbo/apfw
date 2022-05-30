9a6b4124d6aaef8b0078a71d6a6b8ed32ea80b67
Håvard Sørbø <havard@hsorbo.no>
2022-05-30 21:28:12 +0200

    More sources/basebinaries (doc)

--- download-firmwares.py
+++ download-firmwares.py
@@ -3,11 +3,15 @@ import json
 import os
 import requests
 
-# not included
-# https://support.apple.com/kb/DL521?locale=en_GB
-# - https://download.info.apple.com/Mac_OS_X/061-1581.20041220.PtBSE/AirPortExtremeFW5.5.1.basebinary.zip
-# https://support.apple.com/kb/DL522?locale=en_US
-# - https://download.info.apple.com/Mac_OS_X/061-1597.20041220.APTBs/AirPortExpressFW6.1.1.basebinary.zip
+# Not included in updatet-manifest
+
+# Aiport Extreme (original)
+# - 5.5.1 https://download.info.apple.com/Mac_OS_X/061-1581.20041220.PtBSE/AirPortExtremeFW5.5.1.basebinary.zip / https://support.apple.com/kb/DL521?locale=en_GB
+
+# Aiport Express 802.11g (original)
+# - 6.1 / 5.5 (unencrypted) found in AirPort 4.1 for Mac OS X https://support.apple.com/kb/DL536
+# - 6.1.1 https://download.info.apple.com/Mac_OS_X/061-1597.20041220.APTBs/AirPortExpressFW6.1.1.basebinary.zip / https://support.apple.com/kb/DL522?locale=en_US
+# - 6.0 http://download.info.apple.com/Mac_OS_X/061-1574.20041122.ax60f/Express_6.0.basebinary  / https://support.apple.com/kb/DL550?locale=en_GB
 
 prefix = "basebinaries"
 xmlfile = os.path.join(prefix, 'version.xml')
