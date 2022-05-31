f3007aa20d3bf7109e183cbb917bb2ce7ea74a9f
Håvard Sørbø <havard@hsorbo.no>
2022-05-31 20:51:03 +0200

    k

--- ax54g/get_fw.sh
+++ ax54g/get_fw.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-aria2c http://download.info.apple.com/Mac_OS_X/061-1574.20041122.ax60f/Express_6.0.basebinary
-../bbtool.py --extract Express_6.0.basebinary --output 6.0.raw
+curl -L http://download.info.apple.com/Mac_OS_X/061-1574.20041122.ax60f/Express_6.0.basebinary -o 6.0.basebinary
+curl -L http://apsu.apple.com/data/102/061-3062.20070321.omIpl/6.3.basebinary -o 6.3.basebinary
+
+../bbtool.py --extract 6.0.basebinary --output 6.0.raw
 binwalk --extract 6.0.raw
-cp _6.0.raw.extracted/8681 6.0.firmware
-rm -rf _6.* 6.0.raw Express_6.0.basebinary
+cp _6.0.raw.extracted/8681 6.0.vxworks
+
+../bbtool.py --extract 6.3.basebinary --output 6.3.raw
+binwalk --extract 6.3.raw
+cp _6.3.raw.extracted/54C1 6.3.vxworks
+
+rm -rf _6.*
